c5f9456cbc7af997d82dc19c1a6cf7c257c729ce
julinbestina <julin.bestina@yahoo.com>
2022-06-29 13:25:37 +0530

    Added Test case to Validate minimum fare

--- src/main/java/com/bl/cab/InvoiceGenerator.java
+++ src/main/java/com/bl/cab/InvoiceGenerator.java
@@ -1,5 +1,10 @@
 package com.bl.cab;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InvoiceGenerator {
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
@@ -7,7 +12,9 @@ public class InvoiceGenerator {
 
 
     public double calculateFare(double distance, int time) {
-        double fare = distance * COST_PER_KM + time *COST_PER_MIN;
-        return fare;
+        double totalFare = distance * COST_PER_KM + time *COST_PER_MIN;
+        if(totalFare < MIN_FARE)
+            return MIN_FARE;
+        return totalFare;
     }
 }
--- src/test/java/com/bl/cab/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bl/cab/CabInvoiceGeneratorTest.java
@@ -7,7 +7,14 @@ public class CabInvoiceGeneratorTest {
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double totalFare = invoiceGenerator.calculateFare(10, 15);
-        Assert.assertEquals(115,totalFare, 0.0);
+        double fare = invoiceGenerator.calculateFare(10, 15);
+        Assert.assertEquals(115, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenLessThan10_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fare = invoiceGenerator.calculateFare(0.3, 1);
+        Assert.assertEquals(5,fare,0.0);
     }
 }
